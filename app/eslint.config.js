29d56f9511d0dedccbcdf2699675fad99527cee7
Jo <jon.amar@gmail.com>
2026-05-26 15:58:11 -0400

    chore: keep dependency verification stable

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'ios/build', 'tmp']),
+  globalIgnores(['dist', 'ios/build', 'tmp', 'fastlane/derived_data']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
